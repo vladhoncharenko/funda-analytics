5dc27c444a30efa7021f648a6d7b5eb482adb455
Vladyslav Honcharenko <vladyslav.honcharenko@ratesetter.com>
2023-11-28 14:32:10 +0100

    Unit tests base projects were added

---
+++ FundaAnalytics/CacheClientTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CacheClientTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ FundaAnalytics/PartnerApiClientTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PartnerApiClientTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
